eb8fd2d6cbb3658eb45a71c7b0771e4cb31eb1e0
Drakies <58969005+Drakies3172@users.noreply.github.com>
2020-11-09 17:34:27 +0700

    do somting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(nq%s^gry@r64)33ho5ikamawmqi9&+b1=dxnf9%32jfw8=4b1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
